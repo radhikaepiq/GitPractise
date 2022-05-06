27d59a0b4ce159aafc89c67beac7392a6c2a9f2e
radhikaN <radhika@gmail.com>
2022-05-06 11:12:33 +0530

    this is for addition of num4 for sum

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -2,9 +2,10 @@ public class AddTwoNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, num3 = 15, sum;
-      sum = num1 + num2 + num3;
+      int num1 = 5, num2 = 15, num3 = 15,num4 = 4, sum;
+      sum = num1 + num2 + num3 +num4;
 
       System.out.println("Sum of these numbers: "+sum);
    }
 }
+
