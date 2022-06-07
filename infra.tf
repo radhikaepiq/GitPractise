9a8b73bc212ae82dff174bcec1c9626f0c3c6db0
radhikaN <radhika@gmail.com>
2022-06-07 15:11:01 +0530

    instance

--- infra.tf
+++ infra.tf
@@ -3,8 +3,6 @@ resource "aws_spot_instance_request" "test_rad" {
   spot_price             = "0.016"
   instance_type          = "t2.micro"
   spot_type              = "one-time"
-  block_duration_minutes = "120"
-  wait_for_fulfillment   = "true"
   key_name               = "SSHKEY_Rad"
 tags = {
     Name = "radspotinst"
