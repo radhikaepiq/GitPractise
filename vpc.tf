09b91eb342f741dab742c52e3fe823d774dd04a4
radhikaepiq <103490335+radhikaepiq@users.noreply.github.com>
2022-06-07 14:33:16 +0530

    Delete vpc.tf

--- vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "172.30.12.0/24"
-  instance_tenancy = "default"
-  tags = {
-    Name = "VPC-demo_rad"
-    Purpose = "terrafrom using Jenkins"
-  }
-}
