907a228eab84c59244963c5c29815f0980ea9236
radhikaN <radhika@gmail.com>
2022-06-07 14:33:55 +0530

    subnets

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,45 @@ resource "aws_vpc" "main" {
     Purpose = "terrafrom using Jenkins"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+
+vpc_id = aws_vpc.vpc_rad.id
+
+cidr_block ="172.30.12.0/25"
+
+map_public_ip_on_launch ="true"
+
+availability_zone = "ap-south-1"
+
+
+
+tags = {
+
+    Name = "Public SubnetRN"
+
+}
+
+}
+
+
+
+resource "aws_subnet" "private_subnet" {
+
+vpc_id = aws_vpc.vpc_rad.id
+
+cidr_block ="172.30.12.128/25"
+
+map_public_ip_on_launch ="false"
+
+availability_zone = "ap-south-1"
+
+
+
+tags = {
+
+    Name = "Private SubnetRK"
+
+}
+
+}
